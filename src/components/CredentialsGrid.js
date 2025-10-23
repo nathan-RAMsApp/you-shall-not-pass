c3a8c374849ad8defd34e0f9ff8cf5176bc7ffcd
nathan-RAMsApp <nathan@rams-app.com>
2025-10-23 11:53:43 +0100

    Added a credential search and filter functionality to the CredentialsGrid component.

--- src/components/CredentialsGrid.js
+++ src/components/CredentialsGrid.js
@@ -3,6 +3,7 @@ import CredentialItem from "./CredentialItem";
 
 export default function CredentialsGrid() {
     const [credentials, setCredentials] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
     useEffect(() => {
         fetch("../DEMO_DATA/CREDENTIALS.json")
@@ -10,9 +11,30 @@ export default function CredentialsGrid() {
             .then((data) => setCredentials(data));
     }, []);
 
+    const handleSearch = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const searchedCredentials = credentials.filter(
+        (credential) =>
+            credential.company
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase()) ||
+            credential.provider.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
         <div className="credentials-grid container">
-            {credentials.map((credential) => (
+            <div className="container credentials-search">
+                <input
+                    type="search"
+                    className="search-input"
+                    placeholder="Search credentials..."
+                    onInput={handleSearch}
+                    value={searchTerm}
+                />
+            </div>
+            {searchedCredentials.map((credential) => (
                 <CredentialItem key={credential.id} credential={credential} />
             ))}
         </div>
