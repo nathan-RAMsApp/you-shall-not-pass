802f7cb63d2c026ff607bb4e3e7f099e1cc558fc
nathan-RAMsApp <nathan@rams-app.com>
2025-10-23 15:02:21 +0100

    Added routing to create second page for viewing a credential

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import Header from "./components/Header";
 import CredentialsGrid from "./components/CredentialsGrid";
 import InfoComponent from "./components/InfoComponent";
 import "./App.css";
+//import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router";
+import CredentialPage from "./components/CredentialPage";
+import CredentialsContext from "./context/CredentialsContext";
 
 function App() {
     return (
-        <div className="App">
+        <Router>
             <Header />
+            <CredentialsContext.Provider value={[]}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route
+                        path="/credential/:credentialID"
+                        element={<CredentialPage />}
+                    />
+                </Routes>
+            </CredentialsContext.Provider>
+        </Router>
+    );
+}
+
+//setup page route functions
+function Home() {
+    return (
+        <div>
             <main>
                 <div className="container">
                     <InfoComponent />
@@ -15,6 +36,10 @@ function App() {
             </main>
         </div>
     );
+}
+
+function viewCredential({ credentialID }) {
+    return <CredentialPage credentialID={credentialID} />;
 }
 
 export default App;
