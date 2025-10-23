116c3e7d5ac9ce92e129c442a65d7cf6101fec17
nathan-RAMsApp <nathan@rams-app.com>
2025-10-23 14:57:48 +0100

    Created a getFromJSON function that takes a path, list of fields to return and ID to be able to get without a large fetch call each time

--- src/components/CredentialPage.js
+++ src/components/CredentialPage.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router";
+import CredentialsContext from "../context/CredentialsContext";
+import { getFromJSON } from "../utilities/jsonLikeSQL";
 
-export default function CredentialPage({ credential }) {
+export default function CredentialPage() {
+    const { credentialID } = useParams();
+    const [credential, setCredential] = useState(null);
+
+    useEffect(() => {
+        const fetchCredential = async () => {
+            const result = await getFromJSON(
+                "../DEMO_DATA/CREDENTIALS.json",
+                ["id", "company", "provider", "username"],
+                parseInt(credentialID)
+            );
+            setCredential(result);
+        };
+        fetchCredential();
+    }, [credentialID]);
+
+    if (!credential) return <div>Loading...</div>;
+
     return (
-        <div className="credential-item card container">
+        <div className="credential-card card container">
             <h2>{credential.company}</h2>
             <p className="credential-provider">{credential.provider}</p>
             <div className="credential-details">
-                <label for="username">Username:</label>
+                <label htmlFor="username">Username:</label>
                 <input
                     type="text"
                     name="username"
                     value={credential.username}
+                    disabled
+                ></input>
+                <label htmlFor="password">Password:</label>
+                <input
+                    id="credential-password"
+                    type="text"
+                    name="password"
+                    value="********"
                     disabled
                 ></input>
             </div>
